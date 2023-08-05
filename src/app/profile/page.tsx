523e66d9bed98af3b82bbaf88f59da57dae66b60
JitinKGoyal <jkgoyal85@gmail.com>
2023-08-05 19:21:39 +0530

    test commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Profile() {
     return (
         <div className='flex items-center justify-center min-h-screen'>
-            <div>Profile</div>
+            <div>Profile test</div>
         </div>
     )
 }
